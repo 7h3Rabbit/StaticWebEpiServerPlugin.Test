fc5980ea3a52cf4026e0b93fbd9296fbc6c9d939
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-09-26 12:18:27 +0200

    initial commit, playing around with Selenium

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StaticWebEpiServerPlugin.Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automatic testing of StaticWeb EpiServer Plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("7h3Rabbit")]
 [assembly: AssemblyProduct("StaticWebEpiServerPlugin.Test")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyCopyright("Copyright © 7h3Rabbit 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
