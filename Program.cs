09bc75cc6f92cc1a1ac266a8c5ba1819367eb2b4
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-09-26 14:31:25 +0200

    Created page and block test (is page generated correctly)
    - Single Page Publishing Test (Is page generated correctly?)
    - Single Block Publishing Test (Is page generated correctly?)

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading;
 
 namespace StaticWebEpiServerPlugin.Test
@@ -20,17 +21,110 @@ namespace StaticWebEpiServerPlugin.Test
             EdgeDriver driver = CreateBrowser();
             try
             {
+                Console.Clear(); // Remove Selenium debug messages
+                Console.WriteLine("*****************************");
+
                 BackupOutputFolderAndCleanIt();
 
                 LoginToCMS(driver);
 
-                ChangeAlloyPlanPageTitle(driver);
+                SinglePagePublishingTest(driver);
+
+                SingleBlockPublishingTest(driver);
+
+                Console.WriteLine("*****************************");
+
+                // show result for 60 seconds
+                Thread.Sleep(60 * 1000);
             }
             catch (Exception ex)
             {
             }
             driver.Close();
             driver.Quit();
+        }
+
+        private static void SingleBlockPublishingTest(EdgeDriver driver)
+        {
+            string newTitle = "TEST-" + Guid.NewGuid();
+            var errorMessage = ChangeAlloyMeetBlockHeading(driver, newTitle, "Sharing worldwide");
+            if (string.IsNullOrEmpty(errorMessage))
+            {
+                // Verify markup written
+                var alloyMeetFile = outputFolder + @"en\alloy-meet\index.html";
+                if (File.Exists(alloyMeetFile))
+                {
+                    var alloyPlanMarkup = File.ReadAllText(alloyMeetFile, Encoding.UTF8);
+                    if (alloyPlanMarkup.IndexOf(newTitle) > 0)
+                    {
+                        // TEST SUCCESS
+                        Console.WriteLine("Single Block Publishing Test - Alloy Meet Heading - Success");
+                    }
+                    else
+                    {
+                        // TEST FAILED
+                        Console.WriteLine("Single Block Publishing Test - Alloy Meet Heading - FAIL (Generated html file has wrong content, probably not written or wrote old content)");
+                    }
+                }
+                else
+                {
+                    // TEST FAILED
+                    Console.WriteLine("Single Block Publishing Test - Alloy Plan Heading - FAIL (No generated html file found after publish, file: " + alloyMeetFile + ")");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Single Block Publishing Test - Alloy Plan Heading - FAIL (" + errorMessage + ")");
+            }
+
+            Thread.Sleep(5 * 1000);
+
+            // Reset Alloy Plan page (So we can do the same test later)
+            ChangeAlloyMeetBlockHeading(driver, "Sharing worldwide");
+
+            Thread.Sleep(5 * 1000);
+        }
+
+
+        private static void SinglePagePublishingTest(EdgeDriver driver)
+        {
+            string newTitle = "TEST-" + Guid.NewGuid();
+            var errorMessage = ChangeAlloyPlanPageTitle(driver, newTitle, "Alloy Plan");
+            if (string.IsNullOrEmpty(errorMessage))
+            {
+                // Verify markup written
+                var alloyPlanFile = outputFolder + @"en\alloy-plan\index.html";
+                if (File.Exists(alloyPlanFile))
+                {
+                    var alloyPlanMarkup = File.ReadAllText(alloyPlanFile, Encoding.UTF8);
+                    if (alloyPlanMarkup.IndexOf(newTitle) > 0)
+                    {
+                        // TEST SUCCESS
+                        Console.WriteLine("Single Page Publishing Test - Alloy Plan Title - Success");
+                    }
+                    else
+                    {
+                        // TEST FAILED
+                        Console.WriteLine("Single Page Publishing Test - Alloy Plan Title - FAIL (Generated html file has wrong content, probably not written or wrote old content)");
+                    }
+                }
+                else
+                {
+                    // TEST FAILED
+                    Console.WriteLine("Single Page Publishing Test - Alloy Plan Title - FAIL (No generated html file found after publish, file: " + alloyPlanFile + ")");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Single Page Publishing Test - Alloy Plan Title - FAIL (" + errorMessage + ")");
+            }
+
+            Thread.Sleep(5 * 1000);
+
+            // Reset Alloy Plan page (So we can do the same test later)
+            ChangeAlloyPlanPageTitle(driver, "Alloy Plan");
+
+            Thread.Sleep(5 * 1000);
         }
 
         private static void BackupOutputFolderAndCleanIt()
@@ -55,7 +149,8 @@ namespace StaticWebEpiServerPlugin.Test
                         if (EnableBackup)
                         {
                             subDirectory.MoveTo(backupDir.FullName + Path.DirectorySeparatorChar + subDirectory.Name);
-                        }else
+                        }
+                        else
                         {
                             subDirectory.Delete(true);
                         }
@@ -88,7 +183,53 @@ namespace StaticWebEpiServerPlugin.Test
             Thread.Sleep(2 * 1000);
         }
 
-        private static void ChangeAlloyPlanPageTitle(EdgeDriver driver)
+        private static string ChangeAlloyMeetBlockHeading(EdgeDriver driver, string newTitle, string verifyOldHeading = null)
+        {
+            driver.Navigate().GoToUrl("http://localhost:49822/EPiServer/CMS/#context=epi.cms.contentdata:///64&viewsetting=viewlanguage:///en");
+            Thread.Sleep(2 * 1000);
+
+            EnsureAllPropertyViewIsUsed(driver);
+
+            IWebElement propertyElement = GetPropertyElement(driver, "heading");
+            if (propertyElement != null)
+            {
+                var oldValue = propertyElement.GetAttribute("value");
+
+                var verifyTitleBeforeContinue = !string.IsNullOrEmpty(verifyOldHeading);
+                if (verifyTitleBeforeContinue)
+                {
+                    if (oldValue != verifyOldHeading)
+                    {
+                        return "Prerequirement not met for test, block heading is not '" + verifyOldHeading + "'";
+                    }
+                }
+
+                propertyElement.SendKeys(Keys.Control + "A");
+                propertyElement.SendKeys(Keys.Delete);
+                propertyElement.SendKeys(newTitle);
+                Thread.Sleep(1 * 1000);
+
+                propertyElement.SendKeys(Keys.Tab);   // Activates saving of our change
+                Thread.Sleep(2 * 1000);
+
+                driver.Navigate().Refresh();
+                Thread.Sleep(2 * 1000);
+
+                PublishPage(driver);
+
+                Thread.Sleep(1 * 1000);
+
+                driver.Navigate().Refresh();
+                //driver.Navigate().GoToUrl("http://localhost:49822/EPiServer/CMS/#context=epi.cms.contentdata:///64&viewsetting=viewlanguage:///en");
+
+                Thread.Sleep(5 * 1000);
+                return null;
+            }
+
+            return "Unable to find 'Name' property field";
+        }
+
+        private static string ChangeAlloyPlanPageTitle(EdgeDriver driver, string newTitle, string verifyOldTitle = null)
         {
             driver.Navigate().GoToUrl("http://localhost:49822/EPiServer/CMS/#context=epi.cms.contentdata:///6&viewsetting=viewlanguage:///en");
             Thread.Sleep(2 * 1000);
@@ -100,39 +241,46 @@ namespace StaticWebEpiServerPlugin.Test
             {
                 var oldValue = propertyElement.GetAttribute("value");
 
-                if (oldValue == "Alloy Plan")
+                var verifyTitleBeforeContinue = !string.IsNullOrEmpty(verifyOldTitle);
+                if (verifyTitleBeforeContinue)
                 {
-                    string newValue = "TEST-" + Guid.NewGuid();
-
-                    propertyElement.SendKeys(Keys.Control + "A");
-                    propertyElement.SendKeys(Keys.Delete);
-                    propertyElement.SendKeys(newValue);
-                    Thread.Sleep(1 * 1000);
-                    propertyElement.SendKeys(Keys.Tab);   // Activates saving of our change
-
-                    driver.Navigate().Refresh();
-                    Thread.Sleep(2 * 1000);
-
-                    PublishPage(driver);
+                    if (oldValue != verifyOldTitle)
+                    {
+                        return "Prerequirement not met for test, page name is not 'Alloy Plan'"; // Aborting TEST, wrong page OR wrong initial value. Name should be = 'Alloy Plan'
+                        //System.Diagnostics.Process.Start("http://localhost:49822/EPiServer/CMS/#context=epi.cms.contentdata:///6&viewsetting=viewlanguage:///en");
+                        //throw new Exception("Aborting TEST, wrong page OR wrong initial value. Name should be = 'Alloy Plan'");
+                    }
                 }
-                else
-                {
-                    System.Diagnostics.Process.Start("http://localhost:49822/EPiServer/CMS/#context=epi.cms.contentdata:///6&viewsetting=viewlanguage:///en");
-                    throw new Exception("Aborting TEST, wrong page OR wrong initial value. Name should be = 'Alloy Plan'");
-                }
+
+                propertyElement.SendKeys(Keys.Control + "A");
+                propertyElement.SendKeys(Keys.Delete);
+                propertyElement.SendKeys(newTitle);
+                Thread.Sleep(1 * 1000);
+
+                propertyElement.SendKeys(Keys.Tab);   // Activates saving of our change
+                Thread.Sleep(2 * 1000);
+
+                driver.Navigate().Refresh();
+                Thread.Sleep(2 * 1000);
+
+                PublishPage(driver);
 
                 Thread.Sleep(1 * 1000);
+
+                driver.Navigate().Refresh();
+                //driver.Navigate().GoToUrl("http://localhost:49822/EPiServer/CMS/#context=epi.cms.contentdata:///6&viewsetting=viewlanguage:///en");
+
+                Thread.Sleep(5 * 1000);
+                return null;
             }
 
-            driver.Navigate().GoToUrl("http://localhost:49822/EPiServer/CMS/#context=epi.cms.contentdata:///6&viewsetting=viewlanguage:///en");
-
-            Thread.Sleep(5 * 1000);
+            return "Unable to find 'Name' property field";
         }
 
         private static IWebElement GetPropertyElement(EdgeDriver driver, string propertyName)
         {
             var index = 0;
-            while (index < 20)
+            while (index < 50)
             {
                 var actions = new Actions(driver);
                 actions.SendKeys(Keys.Tab).Build().Perform();
@@ -230,6 +378,10 @@ namespace StaticWebEpiServerPlugin.Test
             {
                 // action element found, activate it to change to "all properties" view
                 actionElement.SendKeys(Keys.Space);
+                Thread.Sleep(2 * 1000);
+            }
+            else {
+                driver.Navigate().Refresh();
                 Thread.Sleep(2 * 1000);
             }
         }
